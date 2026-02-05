0c3da0d17666188ccd8a6664f357bd117fcd7624
Gabriel-Gerhardt <gabrielgerhardt27@gmail.com>
2026-02-05 15:05:10 -0300

    it is what it is

--- library/src/main/java/org/plateau/instance/factory/SingletonInstanceFactory.java
+++ library/src/main/java/org/plateau/instance/factory/SingletonInstanceFactory.java
@@ -3,14 +3,18 @@ package org.plateau.instance.factory;
 import java.lang.reflect.InvocationTargetException;
 
 public class SingletonInstanceFactory {
-    private Class<?> clazz;
-    public Object createSingletonInstance(String module){
+    private SingletonInstanceFactory(){
+    }
+    private static Object createSingletonInstance(String module){
         try {
-            this.clazz = Class.forName(module);
-            return clazz.getDeclaredConstructor().newInstance();
+            Class<?> instance = Class.forName(module);
+            return instance.getDeclaredConstructor().newInstance();
         } catch (ClassNotFoundException | InvocationTargetException | InstantiationException | IllegalAccessException |
                  NoSuchMethodException e) {
             throw new RuntimeException(e);
         }
+    }
+    public static Object getInstance(String module){
+        return createSingletonInstance(module);
     }
 }
